ba9868cf88d18ebc14053ce664d09ea77285a7cd
João Quintiliano <201900287@estudantes.ips.pt>
2024-02-16 12:34:32 +0000

    descrição de métodos e TODO list

--- procura.lisp
+++ procura.lisp
@@ -1,2 +1,5 @@
 ;;; Ficheiro procura.lisp
 ;; Implementação dos algoritmos de procura BFS, DFS, A*
+
+; Escrever depois num ficheiro a solução encontrada, número de nós gerados 
+; número de nós expandidos, penetrância, fator de ramificação média e tempo de execução.
--- projeto.lisp
+++ projeto.lisp
@@ -1,3 +1,4 @@
 ;;; Ficheiro projeto.lisp
 ;; Carrega outros ficheiros de código, escreve e lê ficheiros, 
-;;e trata da interação com o utilizador
+;; e trata da interação com o utilizador
+
